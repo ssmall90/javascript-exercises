33065c691b32a857d4813160fae25d9bc1650671
Sheldon Small <ssmall9595@icloud.com>
2022-07-26 14:35:15 +0100

    02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
+   let repeatedString = ''; 
+
+   if (num <= -1) {
+    return 'ERROR'
+   }
+
+    while (num > 0) {
+     repeatedString += string;
+        num--;
+     }
+     return repeatedString;
+    }
+
 
 // Do not edit below this line
 module.exports = repeatString;
