244380bcd16524b30e73580563b57e8faf99ff97
Sheldon Small <ssmall9595@icloud.com>
2022-07-27 14:17:16 +0100

    06_leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    let finalAnswer;
+    if (((year % 4 === 0) && (year % 100 !==0)) || (year % 400 === 0)) {
+      finalAnswer = true
+    } else finalAnswer = false;
+     //console.log (finalAnswer)
+    return finalAnswer
+}
 
 // Do not edit below this line
 module.exports = leapYears;
