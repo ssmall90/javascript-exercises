69c45fcdb6c1dfe2ca8c5cdb6256c6e6d9841882
Sheldon Small <ssmall9595@icloud.com>
2022-07-27 14:49:15 +0100

    07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
+const ctof = function(num) {
+  let formula = (num * 1.8) + 32 
+  let finalAnswer = Math.round(formula * 10) / 10
+  //console.log(finalAnswer)
+  return finalAnswer
+}
+//ctof (21)
 
-};
 
-const ctof = function() {
+const ftoc = function(num) {
+  let formula = (num - 32) / 1.8
+  let finalAnswer = Math.round(formula * 10) / 10
+  //console.log (finalAnswer)
+  return finalAnswer
+}
 
-};
+//ftoc(102)
 
 // Do not edit below this line
 module.exports = {
