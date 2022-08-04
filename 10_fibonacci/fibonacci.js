543ab628f19407cacac6f30c3e09a5e1a50c50b1
Sheldon Small <ssmall9595@icloud.com>
2022-08-04 11:40:46 +0100

    10_fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(fib) {
+    let fibs = [1,1]
+    let fibSum = 0
+    for (let i = 0; i < fib; i++) {
+        fibSum = fibs.at(-1) + fibs.at(-2)
+        fibs.push(fibSum)
+    }
+    if (fib >= 0) {
+        return fibs[fib-1]
+    } else if (fib < 0) {
+        return 'OOPS'
+    }
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
