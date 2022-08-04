32a2aa37177ffe6d7c90b9cba7d4fbf0d0b9295c
Sheldon Small <ssmall9595@icloud.com>
2022-08-04 12:22:18 +0100

    11_getTheTitles completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    const getBooks = arr.map(book => book.title)
+    return getBooks;
 
-};
+} 
 
 // Do not edit below this line
 module.exports = getTheTitles;
