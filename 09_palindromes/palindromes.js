717ee040616fcacce3ca19582fbd15f7c1486ce6
Sheldon Small <ssmall9595@icloud.com>
2022-08-04 11:04:02 +0100

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let stripPunc = str.replace(/[.,/#!$%^&*;:{}]/g, "") 
+    let finalStripPunc = stripPunc.replace(/ /g, '')
+    let lowerCase = finalStripPunc.toLowerCase()
+    let reversed = lowerCase.split('').reverse().join('')
+    if (reversed === lowerCase) {
+        return true
+    } else {
+        return false
+    }
+    //console.log(lowerCase)
 
-};
+    //if (lowerCase.split('').reverse().join('') === lowerCase) {
+     //return true
+    //} else {
+     //return false
+    //}
+ //console.log(str)
+ };
+ 
+ //palindromes('hello')
 
 // Do not edit below this line
 module.exports = palindromes;
